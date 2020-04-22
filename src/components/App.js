0f5f25d5e9cd8c3256c17703ef5312d628b24641
A Thomas Bianchi <atbianchi@gmail.com>
2020-04-21 21:31:54 -0400

    set background color

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      {/* Minutes input */}
+      {/* Logged bar */}
+      {/* Goal set */}
+      {/* Start over */}
+    </div>
+  );
+};
+
+export default App;
